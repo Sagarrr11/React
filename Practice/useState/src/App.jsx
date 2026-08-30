2e808e640f6d3fa2ac141f9c39fd971683e65870
Sagarrr11 <sagarrr0511@gmail.com>
2026-08-30 22:50:23 +0530

    UseState practice

---
+++ Practice/useState/src/App.jsx
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+const App = () => {
+  const [user, setUser] = useState("Rohan")
+  /* user is its read only value and setuser is its write only value by which we can manage the state trough useState */
+  const btnClicked = ()=>{
+    console.log(user)
+    setUser("Sohan")
+  }
+  return (
+    <div className='p-5'>
+      <h1 className='text-3xl mb-2'>{user}</h1>
+      <button onClick={btnClicked} className='rounded-3xl bg-red-950 text-m px-3 py-1 text-white'>Change user</button>
+    </div>
+  )
+}
+
+export default App
